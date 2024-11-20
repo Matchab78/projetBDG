c237f5c017366a8597064fa7d765804eeec3c210
78KLM <kalembaexoce@gmail.com>
2024-11-20 16:01:56 +0100

    ACC FINI

--- templates/script/publicNav.js
+++
@@ -1,6 +0,0 @@
-function toggleDropdown() {
-    const dropdownMenu = document.getElementById('dropdown-menu');
-    const menuToggle = document.querySelector('.menu-toggle');
-    dropdownMenu.classList.toggle('show');
-    menuToggle.classList.toggle('active');
-}
